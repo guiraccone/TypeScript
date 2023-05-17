69d746a0d7b239a0ec2e8ae6965ad9c13b80bf2b
guiraccone <guilherme.raccone@hotmail.com>
2023-05-17 01:05:44 -0300

    functions em typescript

---
+++ ts-basics/functions-test.ts
@@ -0,0 +1,5 @@
+import addNumbers, { addStrings, format, getName } from './functions'
+
+console.log(addNumbers(3, 6))
+console.log(addStrings('3AAAA', 'a'))
+console.log(getName({ first: 'jack', last: 'other'}))
